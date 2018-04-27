f17d5d7eb74991401faf7ef64058d093104af2b3
hueizhe <929494518@qq.com>
2018-04-27 19:58:17 +0800

    new project

---
+++ api/src/main/java/com/github/config/RootConfig.java
@@ -0,0 +1,12 @@
+package com.github.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class RootConfig {
+
+
+
+}
